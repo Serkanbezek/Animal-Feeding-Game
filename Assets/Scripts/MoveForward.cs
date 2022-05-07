1f0dd3dd949e2977362653133a0a2107be1172f7
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-07 15:35:34 +0300

    Update MoveForward.cs

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 40.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float speed = 40.0f;
+    
     // Update is called once per frame
     void Update()
     {
