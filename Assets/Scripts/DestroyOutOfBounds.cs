38d21a996af689390b7325b7936b0cad3a8c5231
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-07 15:35:42 +0300

    Update DestroyOutOfBounds.cs

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,12 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
@@ -19,12 +14,11 @@ public class DestroyOutOfBounds : MonoBehaviour
        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        } 
+       else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
-
-
     }
 }
