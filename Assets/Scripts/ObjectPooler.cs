c8098c1476a0b5bc472113e042e10336b41a2f04
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-07 18:50:51 +0300

    Create ObjectPooler.cs
    Added object pooling instead of instantiating and destroying.

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+    public List<GameObject> PooledObjects;
+    public GameObject ObjectToPool;
+    public int AmountToPool;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Loop through list of pooled objects,deactivating them and adding them to the list 
+        PooledObjects = new List<GameObject>();
+        for (int i = 0; i < AmountToPool; i++)
+        {
+            GameObject obj = Instantiate(ObjectToPool);
+            obj.SetActive(false);
+            PooledObjects.Add(obj);
+            obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        // For as many objects as are in the pooledObjects list
+        for (int i = 0; i < PooledObjects.Count; i++)
+        {
+            // if the pooled objects is NOT active, return that object 
+            if (!PooledObjects[i].activeInHierarchy)
+            {
+                return PooledObjects[i];
+            }
+        }
+        // otherwise, return null   
+        return null;
+    }
+
+}
