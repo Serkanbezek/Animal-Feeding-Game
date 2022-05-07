62bf5fffad05cabfa015ee4c5274cfe470fe7c70
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-07 15:35:09 +0300

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,33 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
-    private float spawnRangeX = 16;
-    private float spawnPosZ = 20;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    public GameObject[] AnimalPrefabs;
+    [SerializeField] private float spawnRangeX = 16;
+    [SerializeField] private float spawnPosZ = 20;
+    [SerializeField] private float startDelay = 2;
+    [SerializeField] private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnAnimalRoutine());
-
     }
     private IEnumerator SpawnAnimalRoutine()
     {
         yield return new WaitForSeconds(startDelay);
         while (true)
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            int animalIndex = Random.Range(0, AnimalPrefabs.Length);
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(AnimalPrefabs[animalIndex], spawnPos, AnimalPrefabs[animalIndex].transform.rotation);
             yield return new WaitForSeconds(spawnInterval);
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-       
-    }
-
 }
