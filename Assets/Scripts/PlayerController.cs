76e90a0f6b45aafaa61cff05e053ff04dd38e466
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-07 18:51:48 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,39 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float HorizontalInput;
-    public float PlayerSpeed = 10.0f;
-    public float Xrange = 15.5f;
-    public GameObject ProjectilePrefab;
-    
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float xRange = 20;
+  
     // Update is called once per frame
     void Update()
     {
-        HorizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * PlayerSpeed * Time.deltaTime * HorizontalInput);
-
         ConstrainPlayer();
+
+        // Player movement left to right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ShootProjectile();
+            // Get an object object from the pool
+            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
+            if (pooledProjectile != null)
+            {
+                pooledProjectile.SetActive(true); // activate it
+                pooledProjectile.transform.position = transform.position; // position it at player
+            }
         }
+
     }
 
     private void ConstrainPlayer()
     {
         var tempPos = transform.position;
-        tempPos.x = Mathf.Clamp(tempPos.x, -Xrange, Xrange);
+        tempPos.x = Mathf.Clamp(tempPos.x, -xRange, xRange);
         transform.position = tempPos;
-    }
-
-    private void ShootProjectile()
-    {
-        Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation);
     }
 }
